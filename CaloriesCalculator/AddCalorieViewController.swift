7892cec422af8e0802173fd7b8f35a64e1b975bf
장정윤 <jeinie9179@naver.com>
2023-06-06 20:54:26 +0900

    floating button 세팅

---
+++ CaloriesCalculator/AddCalorieViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddCalorieViewController.swift
+//  CaloriesCalculator
+//
+//  Created by 장정윤 on 2023/06/06.
+//
+
+import UIKit
+
+class AddCalorieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- CaloriesCalculator/CaloriesViewController.swift
+++ CaloriesCalculator/CaloriesViewController.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 import FSCalendar
+import JJFloatingActionButton
 
 class CaloriesViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate {
 
     @IBOutlet weak var calorieConsumption: UILabel!
-    @IBOutlet weak var targetCalories: UITextField!
+    @IBOutlet weak var targetCalorie: UILabel!
     @IBOutlet weak var fsCalendar: FSCalendar!
+    @IBOutlet weak var calorieList: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,15 @@ class CaloriesViewController: UIViewController, FSCalendarDataSource, FSCalendar
         fsCalendar.dataSource = self
         fsCalendar.delegate = self
         
+    }
+    
+    // 플로팅 버튼 라이브러리 사용
+    func addFloatingButton() {
+        let addBtn = JJFloatingActionButton()
         
     }
+    
+    // add 버튼을 눌렀을 때
+    
 }
+
