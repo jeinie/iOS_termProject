5120eab392466e825542c48a4dc674d8a449835a
jeinie <jeinie9179@naver.com>
2023-06-17 01:16:49 +0900

    시간대별로 칼로리 더한 값 파이어베이스에 저장

--- CaloriesCalculator/AddCalorieViewController.swift
+++ CaloriesCalculator/AddCalorieViewController.swift
@@ -14,6 +14,8 @@ class AddCalorieViewController: UIViewController, UISearchBarDelegate, UITableVi
     let baseURL = "https://openapi.foodsafetykorea.go.kr/api"
     let apiKey = "8e76f22662e143f59385"
     var selectedDate: Date?
+    var month: Int?
+    var day: Int?
     var time: String?
     var isFiltering: Bool = false
     var foodList: [[String: String]] = []
@@ -26,14 +28,18 @@ class AddCalorieViewController: UIViewController, UISearchBarDelegate, UITableVi
 
         // 선택한 날짜
         if let selectedDate = selectedDate {
-            print("선택한 날짜: \(selectedDate)")
-            self.selectedDate = selectedDate
+            let calendar = Calendar.current
+            let month = calendar.component(.month, from: selectedDate)
+            let day = calendar.component(.day, from: selectedDate)
+            
+            print("선택한 날짜(월, 일): \(month)월 \(day)일")
+            self.month = month
+            self.day = day
         }
         // 선택한 시간
         if let time = time {
             self.title = time
         }
-        
         
         searchBar.delegate = self
         searchBar.showsCancelButton = true
@@ -137,7 +143,7 @@ extension AddCalorieViewController {
         tableView.deselectRow(at: indexPath, animated: true) // 선택된 셀 바로 deselect (회색으로 유지되는 거 방지)
         let actionSheet = UIAlertController(title: "칼로리 추가하기", message: "추가하시겠습니까?", preferredStyle: .actionSheet)
         actionSheet.addAction(UIAlertAction(title: "추가하기", style: .default, handler: {(ACTION:UIAlertAction) in
-            print("추가하기 선택")
+            print(">> 추가하기 선택")
             
             // 파이어베이스에 선택한 셀의 데이터 추가
             let selectedCell = tableView.cellForRow(at: indexPath)
@@ -145,15 +151,43 @@ extension AddCalorieViewController {
                 print("선택한 셀의 음식이름: \(foodName)")
                 print("선택한 셀의 칼로리: \(calorie)")
                 let data: [String: Any] = [
-                    "food": foodName,
                     "calorie": calorie
                 ]
                 
-                self.db.collection("calculator").addDocument(data: data) { error in
-                    if let error = error {
-                        print("Firestore 데이터 추가 실패: \(error.localizedDescription)")
+                let date = "\(self.month!)월 \(self.day!)일"
+                let documentRef = self.db.collection(date).document(self.title!)
+                documentRef.getDocument { (document, error) in
+                    // 문서 이미 존재하면, 기존 칼로리에 더함
+                    if let document = document, document.exists {
+                        print("가져온 칼로리: \(document.data()?["calorie"] as! String)")
+                        if let existCalorie = document.data()?["calorie"] as? String {
+                            let extractExistCalorie = existCalorie.replacingOccurrences(of: " Kcal", with: "")
+                            let extractSearchedCalorie = calorie.replacingOccurrences(of: " Kcal", with: "")
+                            if let existCalorieDouble = Double(extractExistCalorie), let searchedCalorie = Double(extractSearchedCalorie) {
+                                var currentCalorie = existCalorieDouble + searchedCalorie
+                                print("선택한 칼로리: \(searchedCalorie)")
+                                print("더한 칼로리: \(currentCalorie)")
+                                let resCalorie = String(format: "%.2f", currentCalorie)
+                                let calorieVal = resCalorie + " Kcal"
+                                documentRef.updateData(["calorie": calorieVal]) { error in
+                                    if let error = error {
+                                        print("칼로리 더하기 실패: \(error)")
+                                    } else {
+                                        print("칼로리 더하기 성공")
+                                    }
+                                }
+                            } else {
+                                print("칼로리 double 형 변환 실패")
+                            }
+                        }
                     } else {
-                        print("Firestore 데이터 추가 성공")
+                        documentRef.setData(data) { error in
+                            if let error = error {
+                                print("Firestore 데이터 추가 실패: \(error.localizedDescription)")
+                            } else {
+                                print("Firestore 데이터 추가 성공")
+                            }
+                        }
                     }
                 }
             }
@@ -164,7 +198,5 @@ extension AddCalorieViewController {
         
         // 액션시트 표시
         present(actionSheet, animated: true, completion: nil)
-        
-        
     }
 }
